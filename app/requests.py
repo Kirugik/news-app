00d427e8345d13c4bb0d5cc4f96fe4432cacaf06
Robert Kirui <kirugik79@gmail.com>
2022-05-01 02:01:11 +0300

    Feat: Create function to render 404 error page

--- app/requests.py
+++ app/requests.py
@@ -7,11 +7,15 @@ api_key = None
 # Getting the news base url
 base_url = None
 
+# Getting news secondary url
+secondary_url = None
+
 
 def configure_request(app):
-    global api_key, base_url
+    global api_key, base_url, secondary_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
+    secondary_url = app.config['NEWS_API_SECONDARY_URL'] 
 
 
 def get_sources(category):
