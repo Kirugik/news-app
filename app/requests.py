d5d57dd82eea334c2181341ac99a1c395082ae79
Robert Kirui <kirugik79@gmail.com>
2022-05-02 15:25:49 +0300

    deployment to heroku

--- app/requests.py
+++ app/requests.py
@@ -13,7 +13,7 @@ articles_url = None
 def configure_request(app):
     global api_key, base_url, articles_url
 
-    
+    api_key = '0b5a4302d4aa4e68af45cd08d1e44896'
     # api_key = app.config['NEWS_API_KEY']  
     base_url = app.config['NEWS_API_BASE_URL']
     articles_url = app.config['ARTICLES_URL']
